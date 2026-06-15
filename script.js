38863fbe250d82ccca80b609a167588202e34315
prempaudel <premprakashpaudel6@gmail.com>
2026-06-15 19:17:36 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Computer Science Student", "Frontend Developer", "Web Designer", "Cyber Security Analyst"],
+        strings: ["Computer Science Student", "Frontend Developer", "Web Designer", "Graphics Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Computer Science Student", "Frontend Developer", "Web Designer", "Cyber Security Analyst"],
+        strings: ["Computer Science Student", "Frontend Developer", "Web Designer", "Graphics Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
